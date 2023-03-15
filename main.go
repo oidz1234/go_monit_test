bf22a54b8ddad5c54fcb31d45ec0c373744bab90
Mark McNally <mark@mcnally.je>
2023-03-15 19:32:48 +0000

    get current time at start of script for heartbeat purposes

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
     "fmt"
      "go_monitor/monitors"
+     "time"
 )
 
 func main() {
     fmt.Println("Hello, World!")
+    fmt.Printf("Heartbeat (unix)time %v\n", time.Now().Unix())
 
     monitors.Test()
     monitors.GetHostDetails()
